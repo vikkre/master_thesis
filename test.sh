97dc1332be08a0533bc0457f8cf9d416328f7ab0
Viktor Kretzschmar <viktor.kretzschmar@gmail.com>
2022-09-19 17:04:02 +0200

    add meta renderer

--- test.sh
+++ test.sh
@@ -20,4 +20,7 @@ popd
 # build/RayTrace res/renderer/ddgi.renderer res/scene/cornell_box_with_ball.scene 1280 720
 # build/RayTrace res/renderer/ddgi_median.renderer res/scene/cornell_box_with_ball.scene 1280 720
 
-build/RayTrace res/renderer/meta_normal_vector.renderer res/scene/cornell_box_with_blocks.scene 1280 720
+# build/RayTrace res/renderer/meta_normal_vector.renderer res/scene/cornell_box_with_blocks.scene 1280 720 out/meta_normal_vector.ppm 5
+# build/RayTrace res/renderer/meta_position.renderer res/scene/cornell_box_with_blocks.scene 1280 720 out/meta_position.ppm 5
+# build/RayTrace res/renderer/meta_distance.renderer res/scene/cornell_box_with_blocks.scene 1280 720 out/meta_distance.ppm 5
+# build/RayTrace res/renderer/meta_weights.renderer res/scene/cornell_box_with_blocks.scene 1280 720 out/meta_weights.ppm 5

89f4f36858171715517f13a580b45d07ee24a132
Viktor Kretzschmar <viktor.kretzschmar@gmail.com>
2022-10-30 11:12:00 +0100

    refactor shader

--- src/graphic/renderer/renderer.cpp
+++ src/graphic/renderer/renderer.cpp
@@ -1,8 +1,8 @@
 #include "renderer.h"
 
 
-const std::vector<std::string> Renderer::RMISS_SHADERS = {"v1_miss.spv", "v1_shadow_miss.spv"};
-const std::vector<std::string> Renderer::RCHIT_SHADERS = {"v1_closesthit.spv", "v1_shadow_closesthit.spv"};
+const std::vector<std::string> Renderer::RMISS_SHADERS = {"v1_miss.spv"};
+const std::vector<std::string> Renderer::RCHIT_SHADERS = {"v1_closesthit.spv"};
 
 
 Renderer::Renderer(Device* device)
